789b98f844d8f0f3ceb5d14a3a6a8ce2fbfa8951
Justin Calleja <justin.calleja@mrggroup.com>
2019-05-17 12:03:35 +0200

    part 4

--- src/resolvers.js
+++ src/resolvers.js
@@ -3,7 +3,16 @@ import data from "./data.json";
 const resolvers = {
   Query: {
     articles: () => data.articles,
-    users: () => data.users,
+    users: (_parent, { limit }) => {
+      if (limit && limit > 0) {
+        let res = []
+        for (let i = 0; i < limit; i++) {
+          res.push(data.users[i])
+        }
+        return res
+      }
+      return data.users
+    },
     greeting: (_parent, { name }) => (name ? `Hello ${name}` : "Hello world"),
     sum: (_parent, { numbers = [] } = {}) => {
       if (numbers && numbers.length > 0) {
